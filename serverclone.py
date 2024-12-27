57e1cd593348077676091d7a5f50ee10d2aebbee
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:39:00 +0000

    Implement documentation for API authentication

--- serverclone.py
+++ serverclone.py
@@ -175,4 +175,4 @@ class Clone:
                 print_delete(f"Deleted Emoji: {emoji.name}")
             except discord.Forbidden:
                 print_error(f"Error While Deleting Emoji{emoji.name}")
-            except discord.HTTPExc
+            except discord.HTT
