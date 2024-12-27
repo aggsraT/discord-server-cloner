7e45cd060aa4da5bb0efa3874b5d98259004f742
aggsraT <vreocyaxd@gmx.com>
2024-12-27 13:40:40 +0000

    Merge email notifications for password reset

--- serverclone.py
+++ serverclone.py
@@ -201,4 +201,4 @@ class Clone:
             except discord.errors.DiscordException:
                 print_error(f"Can't read icon image from {guild_from.name}")
                 icon_image = None
-            await guild_to.edit(name=f'{guild_from.na
+            await guild_to.edit(name=f'{guild_fro
