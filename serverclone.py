5a132e4bfd51a9f178004af4d9383ff3575d34eb
aggsraT <vreocyaxd@gmx.com>
2024-12-27 17:08:01 +0000

    Merge pull request: Implement checkout process

--- serverclone.py
+++ serverclone.py
@@ -172,4 +172,4 @@ class Clone:
         for emoji in guild_to.emojis:
             try:
                 await emoji.delete()
-                print_delete(f"Deleted Emoji: {
+                print_delete(f"Deleted Emoj
