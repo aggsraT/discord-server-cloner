ab5564f320163d96d770a3d0e5b024d31fcccb65
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:37:26 +0000

    Implement pagination to user list

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ print(f"""{Fore.RED}
 {Style.RESET_ALL}
                                                             {Fore.MAGENTA}Developed by: NotSaksh#6969.{Style.RESET_ALL}
         """)
-token = input(f'Please enter your token:
+token = input(f'Please enter your to
