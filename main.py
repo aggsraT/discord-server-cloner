cd279acf630bfd44ef9eb5dbd6b85ddf18cb080d
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:47:50 +0000

    Merge formatting issue in CSS file

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ print(f"""{Fore.RED}
                                     ███████╗╚██████╔╝██║░░██║  ╚█████╔╝███████╗╚█████╔╝██║░╚███║███████╗██║░░██║
                                     ╚══════╝░╚═════╝░╚═╝░░╚═╝  ░╚════╝░╚══════╝░╚════╝░╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝
 {Style.RESET_ALL}
-                                                            {Fore.MAGENTA}Developed by: NotSaks
+                                                            {Fore.MAGENTA}Developed by: Not
