27cd62357e6c20a316aaffd197ff330e9fa779a5
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:45:14 +0000

    Fix documentation for deployment process

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ print(f"""{Fore.RED}
                                     ███████╗╚██████╔╝██║░░██║  ╚█████╔╝███████╗╚█████╔╝██║░╚███║███████╗██║░░██║
                                     ╚══════╝░╚═════╝░╚═╝░░╚═╝  ░╚════╝░╚══════╝░╚════╝░╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝
 {Style.RESET_ALL}
-                                                            {Fore.MAGENTA}Developed by: NotSaksh#6969.{Style.RESET_
+                                                            {Fore.MAGENTA}Developed by: NotSaksh#6969.{Style.RE
