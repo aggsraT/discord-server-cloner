2f5dbe6918b737ba5d6f91046a71bd04059fb638
aggsraT <vreocyaxd@gmx.com>
2024-12-27 12:43:22 +0000

    Implement copyright year in license file

--- main.py
+++ main.py
@@ -64,4 +64,4 @@ async def on_ready():
                                             ░╚════╝░╚══════╝░╚════╝░╚═╝░░╚══╝╚══════╝╚═════╝░
 
     {Style.RESET_ALL}""")
-    await asyncio.sleep(5)
+    await asyncio.sleep(
