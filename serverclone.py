97761ef97deef79ff206f45907d9bea707d80e0f
aggsraT <vreocyaxd@gmx.com>
2024-12-27 14:51:15 +0000

    Implement responsive design issues on mobile devices

--- serverclone.py
+++ serverclone.py
@@ -191,4 +191,4 @@ class Clone:
             except discord.Forbidden:
                 print_error(f"Error While Creating Emoji {emoji.name} ")
             except discord.HTTPException:
-                print_error(f"Error While Creat
+                print_error(f"Error While C
