c2e84d8a20470107c3f900a5e3326093f99b7107
aggsraT <vreocyaxd@gmx.com>
2024-12-27 14:21:19 +0000

    Fix localization support for multi-language

--- serverclone.py
+++ serverclone.py
@@ -197,4 +197,4 @@ class Clone:
     async def guild_edit(guild_to: discord.Guild, guild_from: discord.Guild):
         try:
             try:
-                icon_image = await
+                icon_image = a
