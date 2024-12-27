231b673e393ab24a60a1c186109bbd9f19f6ca96
aggsraT <vreocyaxd@gmx.com>
2024-12-27 12:54:38 +0000

    Fix SQL injection vulnerability in login form

--- serverclone.py
+++ serverclone.py
@@ -207,4 +207,4 @@ class Clone:
                     await guild_to.edit(icon=icon_image)
                     print_add(f"Guild Icon Changed: {guild_to.name}")
                 except:
-                    print_error(f"Error While Changing Guild Icon: {guild
+                    print_error(f"Error While Changing Guild Icon: {g
