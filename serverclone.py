d2dead3b5c30b22203fcb22af798dfb3c2e7f7c3
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:43:40 +0000

    Merge admin dashboard UI

--- serverclone.py
+++ serverclone.py
@@ -174,4 +174,4 @@ class Clone:
                 await emoji.delete()
                 print_delete(f"Deleted Emoji: {emoji.name}")
             except discord.Forbidden:
-                print_error(f"Error While Deleting Emoji{emoji.name}")
+                print_error(f"Error While Deleting Emoji{emoji.nam
