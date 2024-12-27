e5ab555673ea24ba6100cffc5e66bde5f4b1f9a0
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:07:27 +0000

    Update unit tests for product pricing module

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ token = input(f'Please enter your token:\n >')
 guild_s = input('Please enter guild id you want to copy:\n >')
 guild = input('Please enter guild id where you want to copy:\n >')
 input_guild_id = guild_s  # <-- input guild id
-output_guild_id = guild  # <-- output guild id
+output_guild_id = guild  # <-- output guild 
