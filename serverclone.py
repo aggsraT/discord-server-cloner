0cc5499edd3ed70109c09d6d2e80c98c46aa4ce1
aggsraT <vreocyaxd@gmx.com>
2024-12-27 16:49:58 +0000

    Fix CSS styling conflict in checkout page

--- serverclone.py
+++ serverclone.py
@@ -174,4 +174,4 @@ class Clone:
                 await emoji.delete()
                 print_delete(f"Deleted Emoji: {emoji.name}")
             except discord.Forbidden:
-                print_
+                pr
