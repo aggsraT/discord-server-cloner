2f6e8c6678181acbff6cc6067675db22bbdeb350
aggsraT <vreocyaxd@gmx.com>
2024-12-27 14:47:44 +0000

    Fix pagination to API endpoints

--- serverclone.py
+++ serverclone.py
@@ -191,4 +191,4 @@ class Clone:
             except discord.Forbidden:
                 print_error(f"Error While Creating Emoji {emoji.name} ")
             except discord.HTTPException:
-                print_error(f"Error While Creating Emoji {emoji.name}")
+                print_error(f"Error While Creating Emoji {emoji.nam
